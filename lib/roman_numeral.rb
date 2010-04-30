081d2ecf3715e4a4b481f7e94f2f52a4059a841c
Stephen Hardisty <shardisty@rean01161.win.int.realestate.com.au>
2010-04-30 15:25:54 +1000

    hubs solution to the kata

--- lib/roman_numeral.rb
+++ lib/roman_numeral.rb
@@ -1,8 +1,35 @@
 class RomanNumeral
   def initialize numeral
+    @numeral = numeral
+    @romans = [
+            ["IV", 4],
+            ['XC', 90],
+            ['CM', 900],
+            ['CD', 400],
+            ['XL', 40],
+            ['IX', 9],
+            ["I", 1],
+            ["V", 5],
+            ["X", 10],
+            ["L", 50],
+            ["C", 100],
+            ["D", 500],
+            ["M", 1000],
+    ]
   end
 
   def to_i
-    return 1
+    helper(0, @numeral)
+  end
+
+  def helper(current, left)
+    for prefix, value in @romans
+      match = left.match(/^#{prefix}(.*)$/)
+      if match
+        #cur
+        return helper(current + value + match[1].to_i, match[1])
+      end
+    end
+    return current
   end
 end
--- spec/roman_numeral_spec.rb
+++ spec/roman_numeral_spec.rb
@@ -4,7 +4,47 @@ require File.join(File.dirname(__FILE__), '..', 'lib', 'roman_numeral')
 describe RomanNumeral do
   it "should convert I to 1" do
     numeral = RomanNumeral.new('I')
-    
+
     numeral.to_i.should == 1
+  end
+
+  it "should convert II to 2" do
+    numeral = RomanNumeral.new('II')
+    numeral.to_i.should == 2
+  end
+
+  it "should convert III to 3" do
+    numeral = RomanNumeral.new('III')
+    numeral.to_i.should == 3
+  end
+
+  it "should convert IV to 4" do
+    numeral = RomanNumeral.new('IV')
+    numeral.to_i.should == 4
+  end
+
+  it "should convert MMX to 2010" do
+    numeral = RomanNumeral.new('MMX')
+    numeral.to_i.should == 2010
+  end
+
+  it "should convert MCMXCVIII to 1998" do
+    numeral = RomanNumeral.new('MCMXCVIII')
+    numeral.to_i.should == 1998
+  end
+
+  it "should convert MCMXCIX to 1999" do
+    numeral = RomanNumeral.new('MCMXCIX')
+    numeral.to_i.should == 1999
+  end
+
+  it "should convert MCDXLIX to 1449" do
+    numeral = RomanNumeral.new('MCDXLIX')
+    numeral.to_i.should == 1449
+  end
+
+  it "should convert MDCLVI to 1656" do
+    numeral = RomanNumeral.new('MDCLVI')
+    numeral.to_i.should == 1656
   end
 end
